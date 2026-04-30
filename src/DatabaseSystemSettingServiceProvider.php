71326b20f68e306b8834429b5e99927953c51c1f
Diogo Alves <diogoalvesdcruz@gmail.com>
2026-04-30 08:50:41 -0300

    Initial commit: skeleton and setup

---
+++ src/DatabaseSystemSettingServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Diogo2550\DatabaseSystemSetting;
+
+use Illuminate\Support\ServiceProvider;
+
+class DatabaseSystemSettingServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void {
+        
+    }
+}
